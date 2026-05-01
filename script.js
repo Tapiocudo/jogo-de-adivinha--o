6b4eac1dda18dac67f0679f6a124d2e730bfa6ee
Pedro Silva <ync.pedro2@gmail.com>
2026-05-01 16:38:51 +0100

    versão final 2.0

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// histórico do jogo
+let historico = [];
+let jogoAtivo = true;
+
+// Sons
+const somAcerto = document.getElementById("somAcerto");
+const somErro = document.getElementById("somErro");
+// quando acertar
+somAcerto.play();
+jogoAtivo = false;
+document.getElementById("reset").style.display = "inline-block";
+//quando errar
+somErro.play();
+
 // Número secreto entre 1 e 100
 let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
@@ -12,6 +26,13 @@ document.getElementById("tentativas").textContent =
   "Tentativas restantes: " + (maxTentativas - tentativas);
 
 function chutar() {
+    historico.push(palpite);
+
+let lista = document.getElementById("historico");
+let item = document.createElement("li");
+item.textContent = palpite;
+lista.appendChild(item);
+
   let input = document.getElementById("palpite");
   let palpite = parseInt(input.value);
   let mensagem = document.getElementById("mensagem");
@@ -49,5 +70,25 @@ function chutar() {
 
 // Finaliza jogo
 function finalizarJogo() {
+    jogoAtivo = false;
+document.getElementById("reset").style.display = "inline-block";
   document.getElementById("palpite").disabled = true;
+}
+
+//impedir jogar após finalizar o jogo
+if (!jogoAtivo) return;
+
+// Reiniciar jogo
+function reiniciarJogo() {
+  numeroSecreto = Math.floor(Math.random() * 100) + 1;
+  tentativas = 0;
+  historico = [];
+  jogoAtivo = true;
+
+  document.getElementById("mensagem").textContent = "";
+  document.getElementById("historico").innerHTML = "";
+  document.getElementById("palpite").disabled = false;
+  document.getElementById("reset").style.display = "none";
+
+  atualizarTentativas();
 }
